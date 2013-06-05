bcc2980095d4cd8776188878a15ffd6d0aaaa15d
Jared Forsyth <jared@jaredforsyth.com>
2013-06-05 17:37:00 -0600

    stangular

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 
-module.exports = {
-  directive: function() {
+/**
+ * This was taken from the angular docs
+ * http://docs.angularjs.org/api/ng.directive:ngModel.NgModelController
+ */
+
+var angular = require('angularjs');
+
+angular.module('contenteditable', [])
+  .directive('contenteditable', function() {
     return {
       restrict: 'A', // only activate on element attribute
       require: '?ngModel', // get a hold of NgModelController
       link: function(scope, element, attrs, ngModel) {
         if(!ngModel) return; // do nothing if no ng-model
- 
+
         // Specify how UI should be updated
         ngModel.$render = function() {
           element.html(ngModel.$viewValue || '');
         };
- 
+
         // Listen for change events to enable binding
         element.bind('blur keyup change', function() {
           scope.$apply(read);
         });
         read(); // initialize
- 
+
         // Write data to the model
         function read() {
           ngModel.$setViewValue(element.html());
         }
       }
     };
-  }
-};
+  });
 
